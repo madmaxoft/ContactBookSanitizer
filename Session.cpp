6cbeec5a4f5b0580b58fa9cc225948e435a25fe0
Mattes D <github@xoft.cz>
2017-10-27 13:04:01 +0200

    Session: React to adding devices.

--- Session.cpp
+++ Session.cpp
@@ -17,7 +17,9 @@ Session::Session():
 
 void Session::addDevice(std::unique_ptr<Device> a_Device)
 {
+	auto dev = a_Device.get();
 	m_Devices.push_back(std::move(a_Device));
+	emit addedDevice(dev);
 }
 
 
